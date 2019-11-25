062d634114c1e5e7fc2a0b1a72258c49e24579c7
Maurice <maurice.aguda28@gmail.com>
2019-11-25 19:33:49 +0800

    Added serviceworker module for PWA

--- resources/coreui/src/index.js
+++ resources/coreui/src/index.js
@@ -23,6 +23,8 @@ import Register from './views/Pages/Register/'
 import Page404 from './views/Pages/Page404/'
 import Page500 from './views/Pages/Page500/'
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render((
   <Router>
     <Switch>
@@ -34,3 +36,8 @@ ReactDOM.render((
     </Switch>
   </Router>
 ), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
